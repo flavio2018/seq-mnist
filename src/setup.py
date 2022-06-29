50609f55ead7cbd5a97c898a7954ceb08a00826e
flavio2018 <flavio.petruzzellis@gmail.com>
2022-06-29 15:52:06 +0200

    Add setup file

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experiments with D-NTM and sequential MNIST.',
+    author='Flavio Petruzzellis',
+    license='',
+)
+
+"""
+Code written thanks to the following resources:
+- https://stackoverflow.com/questions/20928566/conda-installing-local-development-package-into-single-conda-environment
+"""
