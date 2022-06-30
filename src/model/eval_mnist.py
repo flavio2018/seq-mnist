6eed2831aa0a6db6e80cd2e2b4f60787a5a02bbe
flavio2018 <flavio.petruzzellis@gmail.com>
2022-06-30 16:09:26 +0200

    Dump memory readings to disk

--- src/model/eval_mnist.py
+++ src/model/eval_mnist.py
@@ -19,7 +19,7 @@ def test_mnist(cfg):
 
     _, valid_dataloader = get_dataloaders(cfg, rng)
     model = build_model(cfg.model, device)
-    memory_reading_stats = MemoryReadingsStats()
+    memory_reading_stats = MemoryReadingsStats(path=os.getcwd())
 
     logging.info("Starting testing phase")
     valid_accuracy = test_step(device, model, valid_dataloader, memory_reading_stats)
